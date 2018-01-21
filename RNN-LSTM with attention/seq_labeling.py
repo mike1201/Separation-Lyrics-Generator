92d36e1bf4f364f61594b1f59657564165d6ea2e
MinkyuJeong <32096090+mike1201@users.noreply.github.com>
2018-01-21 18:35:35 +0900

    Update seq_labeling.py

--- RNN-LSTM with attention/seq_labeling.py
+++ RNN-LSTM with attention/seq_labeling.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  28 11:32:21 2016
-
-@author: Bing Liu (liubing@cmu.edu)
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
